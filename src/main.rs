9080e24e0e822ddd5c8663da9a9a37c978cb86cb
Insidious Beaver <insidiousbeaver@proton.me>
2023-12-09 23:10:50 +0100

    solved part 2 with range intersection algo + magic numbers makubma

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,86 @@ use std::{
     fs::{self},
     vec,
 };
-type SourceInput = Vec<u64>;
+type SourceRanges = Vec<(i64, i64)>;
+type SourceInput = Vec<i64>;
 #[derive(Debug)]
 struct CategoryMap {
     id: usize,
-    dst: u64,
-    src: u64,
-    length: u64,
+    dst: i64,
+    src: i64,
+    length: i64,
 }
 
 fn main() {
     let input_path = env::var("aoc_2023_05_path").unwrap() + "/input.txt";
     let input = fs::read_to_string(&input_path).unwrap();
 
-    let (mut src, categories_map) = parse_input(input);
-    let mut src_copy = src.clone();
-    let result = calc_part1(&mut src_copy, categories_map);
-    println!("{:?}", result);
+    let (src, categories_map) = parse_input(input);
+
+    let sr = convert_src_to_ranges(&src);
+    let min = calc_part2(sr, categories_map);
+    println!("min: {}", min);
 }
-fn calc_part1(mut src: &mut SourceInput, categories_map: Vec<CategoryMap>) -> u64 {
+fn calc_part2(mut src: SourceRanges, categories_map: Vec<CategoryMap>) -> i64 {
+    let mut temp_src: SourceRanges = vec![];
+
+    for cat_id in 1..8 {
+        'sourceloop: for rng in &mut src {
+            for cat in &categories_map {
+                if cat.id == cat_id {
+                    if rng.0 <= cat.src + cat.length - 1 && cat.src <= rng.0 + rng.1 - 1 {
+                        let dst_src_dif = cat.dst - cat.src;
+                        let a1 = i64::max(rng.0, cat.src);
+                        let a2 = i64::min(rng.0 + rng.1 - 1, cat.src + cat.length - 1);
+                        let a11 = a1 + dst_src_dif;
+                        let a22 = a2 - a1 + 1;
+                        temp_src.push((a11, a22));
+                        rng.0 = rng.0 + a22 - 1;
+                        rng.1 = rng.1 - a22;
+                    }
+                    if rng.0 == 0 && rng.1 == 0 {
+                        continue 'sourceloop;
+                    }
+                }
+            }
+            if rng.1 > 0 {
+                temp_src.push(*rng);
+                rng.0 = 0;
+                rng.1 = 0;
+            }
+        }
+
+        src = temp_src;
+        println!("{:?}", src);
+        temp_src = vec![];
+    }
+    let mut min = i64::MAX;
+    for s in src {
+        if s.0 < min && s.1 > 0 {
+            min = s.0;
+        }
+    }
+    return min;
+}
+fn convert_src_to_ranges(src: &SourceInput) -> SourceRanges {
+    let mut new_src = vec![];
+    let mut pair: Option<(i64, i64)> = None;
+    for n in src {
+        match pair {
+            Some(mut p) => {
+                p.1 = *n;
+                new_src.push(p);
+                pair = None;
+            }
+            None => pair = Some((*n, 0)),
+        }
+    }
+    return new_src;
+}
+fn calc_part1(src: &mut SourceInput, categories_map: Vec<CategoryMap>) -> i64 {
     let mut cat_id = 1;
-    let mut found: Vec<u64> = vec![];
-    let mut not_found: Vec<u64> = vec![];
+    let mut found: Vec<i64> = vec![];
+    let mut not_found: Vec<i64> = vec![];
 
     while cat_id < 8 {
         while src.len() > 0 {
@@ -52,7 +110,7 @@ fn calc_part1(mut src: &mut SourceInput, categories_map: Vec<CategoryMap>) -> u6
     return *result;
 }
 fn parse_input(input: String) -> (SourceInput, Vec<CategoryMap>) {
-    let mut src: Vec<u64> = vec![];
+    let mut src: Vec<i64> = vec![];
     let mut categories_map: Vec<CategoryMap> = vec![];
     let mut cat_id = 0;
 
@@ -66,11 +124,11 @@ fn parse_input(input: String) -> (SourceInput, Vec<CategoryMap>) {
                 if c.is_ascii_digit() {
                     word.push(c);
                     if j == line.len() - 8 && !word.is_empty() {
-                        src.push(u64::from_str_radix(&word, 10).unwrap());
+                        src.push(i64::from_str_radix(&word, 10).unwrap());
                         word.clear();
                     }
                 } else if (c == ' ' || j == line.len() - 1) && !word.is_empty() {
-                    src.push(u64::from_str_radix(&word, 10).unwrap());
+                    src.push(i64::from_str_radix(&word, 10).unwrap());
                     word.clear();
                 }
             }
@@ -92,24 +150,24 @@ fn parse_input(input: String) -> (SourceInput, Vec<CategoryMap>) {
             if c.is_ascii_digit() {
                 word.push(c);
                 if j == line.len() - 1 {
-                    cm.length = u64::from_str_radix(&word, 10).unwrap();
+                    cm.length = i64::from_str_radix(&word, 10).unwrap();
                     word.clear();
                 }
             } else if c == ' ' {
                 if cm_counter == 0 {
                     cm_counter += 1;
                     cm.id = cat_id;
-                    cm.dst = u64::from_str_radix(&word, 10).unwrap();
+                    cm.dst = i64::from_str_radix(&word, 10).unwrap();
                     word.clear();
                 } else if cm_counter == 1 {
                     cm_counter += 1;
 
-                    cm.src = u64::from_str_radix(&word, 10).unwrap();
+                    cm.src = i64::from_str_radix(&word, 10).unwrap();
                     word.clear();
                 } else if cm_counter == 2 {
                     cm_counter += 1;
 
-                    cm.length = u64::from_str_radix(&word, 10).unwrap();
+                    cm.length = i64::from_str_radix(&word, 10).unwrap();
                     word.clear();
                 }
             }
